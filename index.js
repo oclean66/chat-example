a17d14a7467aabc1f5f3b8e1a536d68de2717f82
oclean66 <oclean66@gmail.com>
2023-07-29 14:01:02 -0400

    close

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const app = require('express')();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
+const io = require('socket.io')(http, {
+  cors: {
+    origin: "https://example.com"
+  }
+});
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
